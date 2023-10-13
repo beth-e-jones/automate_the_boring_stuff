adeb25d83e24bf4dd48c4c41bebcbb1fce8dddf8
Beth Jones <beth.e.jones@ons.gov.uk>
2023-10-13 10:55:19 +0100

    finished chapter 1

---
+++ chapter_1_basics.py
@@ -0,0 +1,22 @@
+"""
+Operator        Operation
+**              Exponent
+%               Modulus/remainer
+/ /             Integer division
+/               Division
+*               Multiplication
+-               Subtraction
++               Addition
+
+
+Order - **, *, /, //, %, +, -
+(Use brackets to override order with BIDMAS)
+
+
+Storing Variables:
+Variable is initialised the first time a value is stored in it. When it's 
+assigned a new value, we overwrite the variable. 
+
+Naming variables:
+PEP8 says to use snake case for vatiable names
+"""
