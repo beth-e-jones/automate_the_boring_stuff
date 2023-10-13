861fd181b0ad3978642a35c8723fc73bf28e8752
Beth Jones <beth.e.jones@ons.gov.uk>
2023-10-13 16:00:03 +0100

    working through chapter 2

---
+++ chapter_2_flow_control.py
@@ -0,0 +1,44 @@
+""""
+Flow control:
+Flow control statements tell Python what to do under which conditions
+
+Booleans - True and False
+An integer or floating-point value will always be unequal to a string value
+e.g., 42 == "42" will be False
+
+= is an assignment
+== asks whether 2 values are the same as eachother
+
+Boolean operators are:
+and
+not
+or
+
+and are used to compare Boolean values. 
+Python looks at them in the order 
+not, then and, then or
+
+*And* and *or* are binary operators, and the *and* provides a True output if 
+both are true. Otherwise, it'll output False. 
+
+The *not* operator works with just one Boolean value/expression
+which makes it a unary operator.
+
+
+Flow control 
+Flow control statements start with a condition, then followed by a clause
+Condition = can always evaluate down to a Boolean value of True or False
+A flow control statement makes a decision based on whether condition is True or
+False. 
+
+Lines of code can be grouped into blocks. Blocks have 3 rules:
+Blocks begin when indentation changes, and end when indentation goes back to zero. 
+Blocks can contain other blocks.
+
+Types of flow control statements
+if statement (if, then condition, colon, indented action to take)
+else statement (else, then a colon, then the indented action to take)
+elif (elif, a condition that can evaluate to True/False, colon, indented action)
+while statement ()
+
+"""
