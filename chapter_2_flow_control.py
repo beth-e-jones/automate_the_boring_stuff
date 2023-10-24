4517a044f6f4d5fc683b57749733b7fdb1fa66ad
Beth Jones <beth.e.jones@ons.gov.uk>
2023-10-24 13:38:45 +0100

    chapter 2 exercises

--- chapter_2_flow_control.py
+++ chapter_2_flow_control.py
@@ -39,6 +39,37 @@ Types of flow control statements
 if statement (if, then condition, colon, indented action to take)
 else statement (else, then a colon, then the indented action to take)
 elif (elif, a condition that can evaluate to True/False, colon, indented action)
-while statement ()
+while statement (while, then a condition - true/false, colon, action)
 
+
+Infinite loops:
+Ctrl + C will terminate a programme immediately as a good way of getting out 
+of infinite loop, or interrupt programme in VS Code. 
+
+Break statements:
+A shrotcut to break a while loop early. If Python gets to a break statement, 
+it'll exist the while loop. Just needs to contain the break keyword.
+
+Continue statements:
+Also used inside loops - once the programme reaches a continue statement, it 
+jumps back to the start of a loop and re-evaluates the loop's condition.
+
+Truthy and Falsey variables:
+Essentially true and false booleans
+WHen used in conditions, 0, 0.0 and "" are considered False, everything else 
+is true. 
+
+sys.exit() will stop a function before the last line, if a certain condition
+is met e.g.,
+
+import sys
+
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You typed ' + response + '.')
+    
+    But note that you have to import sys first
 """
